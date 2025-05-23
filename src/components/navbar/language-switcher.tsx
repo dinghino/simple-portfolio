1c2736dd8319c9c64ab76c6c00a97fcd185e7420
dinghino <dinghino@users.noreply.github.com>
2025-05-23 13:29:18 +0200

    ♻️ i18n: improve language switcher

--- src/components/navbar/language-switcher.tsx
+++ src/components/navbar/language-switcher.tsx
@@ -7,11 +7,12 @@ import {
 import { LanguagesIcon } from 'lucide-react'
 import { languages } from '@/data/locales'
 import { Link } from '@/lib/i18n'
-import { usePathname } from 'next/navigation'
 import { Button } from '@/components/ui/button'
+import { useLocale } from 'next-intl'
+import { cn } from '@/lib'
 
 export function LanguageSwitcher() {
-  const pathname = usePathname()
+  const locale = useLocale()
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
@@ -22,8 +23,15 @@ export function LanguageSwitcher() {
       <DropdownMenuContent align="end">
         {languages.map((lang) => (
           <DropdownMenuItem asChild key={lang}>
+            {/* for now we link to `/` since we don't have other pages */}
             <Link locale={lang} href="/" className="flex items-center gap-2 w-full">
-              <span className="font-mono uppercase tracking-wide text-sm">{lang}</span>
+              <span
+                className={cn('font-mono uppercase tracking-wide text-sm', {
+                  'font-bold text-sky-500': lang === locale,
+                })}
+              >
+                {lang}
+              </span>
             </Link>
           </DropdownMenuItem>
         ))}
