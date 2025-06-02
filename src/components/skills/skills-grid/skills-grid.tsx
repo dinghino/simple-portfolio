2b4f9ad13c0c54f09724cd443cee9e1782d3ad8c
dinghino <dinghino@users.noreply.github.com>
2025-06-02 19:41:28 +0200

    🐛 fix skills tabs width w/ scroll-area
    - add scroll area component
    - implement in skills tabs menu

--- src/components/skills/skills-grid/skills-grid.tsx
+++ src/components/skills/skills-grid/skills-grid.tsx
@@ -8,6 +8,7 @@ import { useTranslations } from 'next-intl'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { SkillItem } from '@/components/skills/skill-item'
 import { useStaggerAnimation } from '@/hooks/use-stagger-animation'
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
 
 interface SkillsGridProps {
   skills: SkillsData
@@ -25,17 +26,20 @@ export const SkillsGrid: React.FC<SkillsGridProps> = ({ skills, className }) => 
       onValueChange={(value) => setSelectedCategory(value as SkillCategory)}
       className={cn('w-full', className)}
     >
-      <TabsList className="mb-8 font-mono bg-transparent border-b w-full justify-start rounded-none h-auto p-0">
-        {Object.keys(skills).map((category) => (
-          <TabsTrigger
-            key={category}
-            value={category}
-            className="data-[state=active]:border-b-2 data-[state=active]:border-primary rounded-none px-4 py-2 data-[state=active]:bg-transparent data-[state=active]:shadow-none cursor-pointer"
-          >
-            {t(`categories.${category}`, { default: capitalize(category) })}
-          </TabsTrigger>
-        ))}
-      </TabsList>
+      <ScrollArea className="w-full whitespace-nowrap mb-8 pb-3">
+        <ScrollBar orientation="horizontal" className="mt-8" />
+        <TabsList className="font-mono bg-transparent border-b w-full justify-start rounded-none h-auto p-0">
+          {Object.keys(skills).map((category) => (
+            <TabsTrigger
+              key={category}
+              value={category}
+              className="data-[state=active]:border-b-2 data-[state=active]:border-primary rounded-none px-4 py-2 data-[state=active]:bg-transparent data-[state=active]:shadow-none cursor-pointer"
+            >
+              {t(`categories.${category}`, { default: capitalize(category) })}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+      </ScrollArea>
       {Object.entries(skills).map(([category, skills]) => (
         <TabContent
           key={category}
